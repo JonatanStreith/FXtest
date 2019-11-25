6754549f568d574b66448297105eb1b3cbc96403
JonatanStreith <jonatan@streith.se>
2019-11-25 15:59:28 +0100

    Read up on stuff

--- src/main/java/jonst/App.java
+++ src/main/java/jonst/App.java
@@ -1,11 +1,15 @@
 package jonst;
 
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
@@ -15,22 +19,19 @@ import java.util.Scanner;
 
 /**
  * Hello world!
- *
  */
-public class App extends Application
-{
+public class App extends Application {
     private static Scanner inputReader;
     private int openValue = 0;
     private String gameTitle;
     private String logo;
 
-    public static void main( String[] args )
-    {
+    public static void main(String[] args) {
         inputReader = new Scanner(System.in);
         launch();
     }
 
-    public void init(){     //This runs at the very start of the game!
+    public void init() {     //This runs at the very start of the game!
         openValue = 15;
         gameTitle = "The Amazing Race!";
         System.out.print("Message: ");
@@ -38,9 +39,10 @@ public class App extends Application
 
     }
 
-    public void stop(){     //This runs when the game quits, even if closed down manually
+    public void stop() {     //This runs when the game quits, even if closed down manually
 
         System.out.println(openValue);
+        inputReader.close();
     }
 
     @Override
@@ -49,29 +51,125 @@ public class App extends Application
 
         Group root = new Group();   //This creates a "root" group
 
-        Scene firstScene = new Scene(root);     //This puts firstScene into the root group
+        Group firstDisplay = new Group(root);   //This creates firstDisplay and puts it in the "root" group. We can build trees this way.
+        //Note: root cannot be used to hold scenes now.
+
+        Scene firstScene = new Scene(firstDisplay);     //This puts firstScene into the root group
 
         myStage.setScene(firstScene);       //This passes the firstScene to the Stage
 
-        Canvas canvas = new Canvas(400, 200);       //Canvas object to draw on
-        root.getChildren().add(canvas);     //Adds the canvas to root, apparently. We can still use the canvas name to reach it.
+        Canvas canvas = new Canvas(800, 600);       //Canvas object to draw on
+        firstDisplay.getChildren().add(canvas);     //Adds the canvas to firstDisplay, apparently. We can still use the canvas name to reach it.
+
+        double[] smileyX = {50};
+        double[] smileyY = {50};
+
+
+        //Event handling!
+        firstScene.setOnKeyPressed(
+                new EventHandler<KeyEvent>() {
+                    public void handle(KeyEvent e) {
+                        ;   //Read key pressed
+
+                        if (e.getCode().equals(KeyCode.RIGHT)) {
+                            smileyX[0] += 10;
+                        }
+                        if (e.getCode().equals(KeyCode.LEFT)) {
+                            smileyX[0] -= 10;
+                        }
+                        if (e.getCode().equals(KeyCode.UP)) {
+                            smileyY[0] -= 10;
+                        }
+                        if (e.getCode().equals(KeyCode.DOWN)) {
+                            smileyY[0] += 10;
+                        }
+
+                    }
+                });
+
+
+
+
 
         GraphicsContext gc = canvas.getGraphicsContext2D(); //Not a new object; instead snags the canvas object's graphical details and binds it to the name "gc".
+
 
         gc.setFill(Color.BLUE);
         gc.setStroke(Color.BLACK);
         gc.setLineWidth(2);
-        Font theFont = Font.font( "Times New Roman", FontWeight.BOLD, 48 );
-        gc.setFont( theFont );
-        gc.fillText( "logo", 60, 50 );
-        gc.strokeText( "logo", 60, 50 );
+        Font theFont = Font.font("Times New Roman", FontWeight.BOLD, 48);
+        gc.setFont(theFont);
+        //gc.strokeText( "logo", 70, 60 );
+        //gc.fillText( "logo", 60, 50 );
 
-        Image logo = new Image("file:src/main/java/jonst/logo.png");    //Note annoying path. Can be amended with a URL string, maybe.
-        gc.drawImage(logo, 180,100);
+        //gc.setStroke(Color.GRAY);
+        //gc.strokeText("Hello", 70, 100);
+        //We can write as much new text as we want, anywhere we want; changing the colors and whatnot doesn't change already written stuff.
+
+
+        //Image logo = new Image("file:src/main/java/jonst/logo.png");    //Note annoying path. Can be amended with a URL string, maybe.
+        //gc.drawImage(logo, 180,100);
+
+
+        Image ball = new Image("file:src/main/java/jonst/ball.jpg");
+
+        Image smiley = new Image("file:src/main/java/jonst/smiley.jpg", 50, 50, true, true);
+
+
+        new AnimationTimer() {                           //This creates
+
+
+            final long startNanoTime = System.nanoTime();
+
+            double xDirection = 1;  //If positive, move to the right
+            double yDirection = 1;  //If positive, move downwards
+            double startingX = 0;
+            double startingY = 0;
+            double ballX = startingX;
+            double ballY = startingY;
+
+            double speed = 5;
+
+
+            public void handle(long currentNanoTime) {
+
+                gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+
+
+                gc.strokeText("Wow!", Math.random() * canvas.getWidth(), Math.random() * canvas.getHeight());   //Draw the word "Wow!" at random locations
+
+
+                //double t = (currentNanoTime - startNanoTime) / 100_000_000.0;
+
+                ballX += xDirection * speed;
+                ballY += yDirection * speed;
+
+                if (ballX > canvas.getWidth() - ball.getWidth()) {
+                    xDirection = -1;
+                }
+                if (ballX < 0) {
+                    xDirection = 1;
+                }
+
+                if (ballY > canvas.getHeight() - ball.getHeight()) {
+                    yDirection = -1;
+                }
+                if (ballY < 0) {
+                    yDirection = 1;
+                }
+
+                gc.drawImage(ball, ballX, ballY);
+                gc.drawImage(smiley, smileyX[0], smileyY[0]);
+
+
+                //System.out.println(ballX+", "+ ballY);
+
+            }
+
+        }.start();
 
         myStage.show();     //This initiates the window
     }
-
 
 
 }
